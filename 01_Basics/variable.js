3118db8947c3fcd1ea16d80f2d4e87073cd7abd5
rajuk2649 <rajuk2649@gmail.com>
2026-09-21 13:22:52 +0530

    Add files via upload

---
+++ 01_Basics/variable.js
@@ -0,0 +1,12 @@
+const accountId = 1448
+let accountName = "Anup Pradhan"
+var Password = "123456"
+let accountState = "Active"
+
+
+console.table([accountId,accountName,Password,accountState])
+
+
+/*
+fgfhgjfgj
+*/
